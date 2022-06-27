47cd38382fea11e3cc1f7eab898f3109a2dd98de
bmetenko <bohdanmetenko@outlook.com>
2022-06-27 17:11:02 -0400

    Test python square coordinates

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,10 +1,9 @@
 import unittest
-
 from squares import Square
 
 class TestSquare(unittest.TestCase):
 
-    def test_movement(self):
+    def test_movement_locations(self):
         s1 = Square(side=1)
         self.assertEqual(s1.area, 1)
         self.assertEqual(s1.center, [0.5, 0.5])
@@ -20,6 +19,14 @@ class TestSquare(unittest.TestCase):
         s1.add_xy(1, 1)
         self.assertEqual(s1.area, 1)
         self.assertEqual(s1.center, [2.5, 2.5])  
+        self.assertEqual(
+            s1.coordinates, 
+            [[2, 2], [2, 3], [3, 2], [3, 3]]
+            )
+
+        self.assertEqual(s1.length, 1)
+        self.assertEqual(s1.rotate_times, 1)
+        self.assertEqual(s1.side, 1)
 
         with self.assertRaises(TypeError):
             s1.add_xy("l", "j")
